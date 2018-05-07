fc23b4469eb8e6df7ea2510dcc42b33cd8ba43a0
blckmn <blackman@xtra.com.au>
2018-05-07 21:31:58 +1000

    Changes to the LeadVehicle to make it more inline with Vehicle

--- Csn.Public.Dto/Lead/LeadVehicle.cs
+++ Csn.Public.Dto/Lead/LeadVehicle.cs
@@ -7,28 +7,30 @@ namespace Csn.Public.Dto.Lead
 {
     public class LeadVehicle
     {
-        public string Type { get; set; }            // CAR, BIKE, BOAT, TRUCK, CARAVAN etc
+        /* Type = [CAR, BIKE, BOAT, TRUCK, CARAVAN etc] */
+        public string Type { get; set; }
         public Guid? Identifier { get; set; }
         public string StockNumber { get; set; }
+        public string Url { get; set; }
+
         public string Make { get; set; }
         public string Model { get; set; }
         public string Badge { get; set; }
         public string Series { get; set; }
         public string Description { get; set; }
-        public string Year { get; set; }
+        public SimpleDate ReleaseDate { get; set; }
         public Price Price { get; set; }
-        public string PriceType { get; set; }
 
         public VehicleRegistration Registration { get; set; }
-        public string EngineDescription { get; set; }
         public List<SimpleColour> Colour { get; set; }
 
         public List<TypeValue> IdentificationNumbers { get; set; }
-        public string BodyType { get; set; }
         public SimpleValue<double> Odometer { get; set; }
+
+        public string BodyType { get; set; }
         public string FuelType { get; set; }
-        public string Url { get; set; }
         public string Transmission { get; set; }
+        public string Engine { get; set; }
 
         public string CategoryType { get; set; }
         public string Group { get; set; }
