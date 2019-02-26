1d6f66343b34f9a9729a3b8955a34d075187696f
Davis Zeng <davis.zeng@carsales.office>
2019-02-26 11:25:08 +1100

    added TestDrive Dto including GeoTracking and Infringement

---
+++ Csn.Public.Dto/TestDrive/Infringement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Csn.Public.Dto
+{
+    public class Infringement
+    {
+        public Guid Identifier { get; set; }
+        public Guid? TestDriveIdentifier { get; set; }
+        public string InfringementNumber { get; set; }
+        public double InfringementPenalty { get; set; }
+        public DateTime InfringementDueUtc { get; set; }
+        public double DemeritPoints { get; set; }
+        public DateTime OffenceUtc { get; set; }
+        public string OffenceRegistration { get; set; }
+        public string OffenceDescription { get; set; }
+    }
+}
---
+++ Csn.Public.Dto/TestDrive/Licence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csn.Public.Dto
+{
+    public class Licence
+    {
+        public Guid Identifier { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Address Address { get; set; }
+        public SimpleDate DateOfBirth { get; set; }
+
+        public string LicenceNumber { get; set; }
+        public DateTime LicenceExpiry { get; set; }
+
+        /* CAR, LEARNER, MOTORCYCLE etc. */
+        public string LicenceType { get; set; }
+        public List<LicenceCondition> LicenceConditions { get; set; }
+        public string LicenceSignatureImageUrl { get; set; }
+    }
+
+    public class LicenceCondition
+    {
+        // Using Victorian Licence Conditions as Sample: 
+        // A - automatic transmission
+        // B - synchromesh transmission - heavy vehicle only
+        // E - LAMS restriction, no pillion passenger and zero BAC
+        // I - ignition interlock device
+        // N - automatic transmission(motorcycle)
+        // P - passenger restriction
+        // S - glasses or corrective lenses
+        // V - driver aids or vehicle modifications
+        // X - any condition or restriction VicRoads has advised you of in writing
+        // Z - zero blood alcohol limit
+
+        public Guid Identifier { get; set; }
+        public string Condition { get; set; }
+        public string Description { get; set; }
+    }
+}
