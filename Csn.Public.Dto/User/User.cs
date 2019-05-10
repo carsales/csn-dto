d820082005ef89a0abcaf373bcc11dd06239bf65
ashik.gurung <ashik.gurung@carsales.com.au>
2019-05-10 15:47:37 +1000

    add user dto to support dealer users

---
+++ Csn.Public.Dto/User/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Csn.Public.Dto
+{
+    public class User
+    {
+        public Guid Identifier { get; set; }
+        public string Username { get; set; }
+        public string Status { get; set; }
+        public string EmailAddress { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+        public string Country { get; set; }
+        public string ImageUrl { get; set; }
+        public List<ApplicationClaims> ApplicationClaims { get; set; }
+    }
+
+    public class ApplicationClaims
+    {
+        public string ApplicationName { get; set; }
+        public List<string> Claims { get; set; }
+    }
+}
